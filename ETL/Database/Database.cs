35d89e5e3e10ecbafc4faede2cba1dd1ca74e2dc
Eugene Zaysev <eugene.zaysev@market6.com>
2015-10-19 10:13:30 -0500

    adding etl project

---
+++ ETL/Database/Database.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using MK6.AutomatedTesting.ETL.Extensions;
+
+namespace MK6.AutomatedTesting.ETL.Database
+{
+    public class Database
+    {
+        private static string _connectionString;
+        private static readonly Dictionary<string, string> ResultCache = new Dictionary<string, string>();
+
+
+        public Database(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public static IDbConnection GetConneciton()
+        {
+            var connection = new OdbcConnection(_connectionString);
+            connection.ConnectionTimeout = 120;
+            connection.Open();
+            return connection;
+        }
+
+        public IDbConnection GetConnection()
+        {
+            return GetConneciton();
+        }
+
+        public string ExecuteSQL(string sqlQuery, string sqlSource = "")
+        {
+            string result;
+
+            if (ResultCache.ContainsKey(sqlQuery))
+            {
+                return ResultCache[sqlQuery];
+            }
+
+            using (var con = UtilityExtension.GetConnection(sqlSource))
+            {
+                result = con.ExecuteScalar(sqlQuery).ToString();
+            }
+            ResultCache[sqlQuery] = result;
+            return result;
+
+        }
+    }
+}
---
+++ ETL/Extensions/UtilityExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MK6.AutomatedTesting.ETL.Extensions
+{
+    public static class UtilityExtension
+    {
+        public static IDbConnection GetConnection(string sqlSource)
+        {
+            var connectionString = ConfigurationManager.
+                ConnectionStrings[string.Format("{0}.DBConnString", sqlSource.ToUpperInvariant())].ConnectionString;
+            var connection = new OdbcConnection(connectionString);
+            connection.ConnectionTimeout = 120;
+            connection.Open();
+            return connection;
+        }
+
+        public static string ReplaceParametersFromDictionary(this string startString,
+    IDictionary<string, string> parameters)
+        {
+            var upperCaseParameterDictionary = (from x in parameters
+                                                select x).ToDictionary(x => x.Key.ToUpperInvariant(), x => x.Value);
+
+            var matchString = new Regex(@"@\w*?@");
+
+            Match m = matchString.Match(startString);
+            while (m.Success)
+            {
+                Group g = m.Groups[0];
+                var keySurroundedByAt = g.Captures[0].Value;
+                var keyFound = keySurroundedByAt.Replace("@", string.Empty).ToUpperInvariant();
+                if (upperCaseParameterDictionary.ContainsKey(keyFound))
+                {
+                    startString = startString.Replace(keySurroundedByAt, upperCaseParameterDictionary[keyFound]);
+                }
+
+                m = m.NextMatch();
+            }
+            return startString;
+        }
+    }
+}
